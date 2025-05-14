d558018a922ba9bf7ac0d78c66e8b0b8564152cd
Xarch <rakhaarkhana98@gmail.com>
2025-05-14 19:58:38 +0700

    form Login

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,9 +1,36 @@
+import React, { useState, useEffect } from "react";
 import { Navbar, Container } from "react-bootstrap";
 
 const NavbarComponent = () => {
+  const [showNavbar, setShowNavbar] = useState(true);
+  let lastScrollY = 0;
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > lastScrollY) {
+        setShowNavbar(false); // Jika scroll ke bawah, sembunyikan navbar
+      } else {
+        setShowNavbar(true); // Jika scroll ke atas, tampilkan navbar
+      }
+      lastScrollY = window.scrollY > 0 ? window.scrollY : 0; // Menyimpan posisi scroll terakhir
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   return (
-    <Navbar bg="light" expand="lg" fixed="top">
-      <Container>
+    <Navbar
+      expand="lg"
+      fixed="top"
+      className={`bg-light navbar-transition ${
+        showNavbar ? "" : "navbar-hidden"
+      }`}
+    >
+      <Container fluid>
         <Navbar.Brand>
           <img
             src="/src/assets/logo-ghamcak.png"
@@ -11,7 +38,7 @@ const NavbarComponent = () => {
             width="160"
             height="200"
             className="navbar-logo"
-          />{" "}
+          />
           <span className="text-danger">E</span> -Rapor
         </Navbar.Brand>
       </Container>
--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
 import React from "react";
 import "./App.css";
 
-//import components here
+// import components
 import NavbarComponent from "../components/navbar";
+import LoginForm from "../LoginForm/LoginPage";
 
 function App() {
   return (
     <>
       <NavbarComponent />
+      <div className="mt-5">
+        <LoginForm />
+      </div>
     </>
   );
 }
